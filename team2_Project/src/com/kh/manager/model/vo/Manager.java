ccc93aee78113712da67bda2ef675125c1ff4770
seunghyub <seunghyub.choi@gmail.com>
2023-02-07 20:27:57 +0900

    Update Manager.java

--- team2_Project/src/com/kh/manager/model/vo/Manager.java
+++ team2_Project/src/com/kh/manager/model/vo/Manager.java
@@ -1,5 +1,49 @@
 package com.kh.manager.model.vo;
 
 public class Manager {
+	private int managerNo;
+	private String managerId;
+	private String managerPwd; 
+	
+	public Manager() {
+		
+	}
 
+	public Manager(int managerNo, String managerId, String managerPwd) {
+		super();
+		this.managerNo = managerNo;
+		this.managerId = managerId;
+		this.managerPwd = managerPwd;
+	}
+
+	public int getManagerNo() {
+		return managerNo;
+	}
+
+	public void setManagerNo(int managerNo) {
+		this.managerNo = managerNo;
+	}
+
+	public String getManagerId() {
+		return managerId;
+	}
+
+	public void setManagerId(String managerId) {
+		this.managerId = managerId;
+	}
+
+	public String getManagerPwd() {
+		return managerPwd;
+	}
+
+	public void setManagerPwd(String managerPwd) {
+		this.managerPwd = managerPwd;
+	}
+
+	@Override
+	public String toString() {
+		return "Manager [managerNo=" + managerNo + ", managerId=" + managerId + ", managerPwd=" + managerPwd + "]";
+	}
+	
+	
 }
