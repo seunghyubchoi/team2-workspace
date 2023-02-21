d991d829f85fde63e34429edff75778aece919bd
heeheeyo <hi1004ya@naver.com>
2023-02-21 15:18:05 +0900

    admin login, logout, sidebar
    관리자 로그인, 로그아웃, 사이드바

--- team2_Project/src/com/kh/manager/controller/AdminController.java
+++ team2_Project/src/com/kh/manager/controller/AdminController.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class LogoutController
+ * Servlet implementation class AdminController
  */
-@WebServlet("/logout.ma")
-public class LogoutController extends HttpServlet {
+@WebServlet("/admin.ma")
+public class AdminController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public LogoutController() {
+    public AdminController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,16 +26,14 @@ public class LogoutController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getSession().invalidate();
-		response.sendRedirect(request.getContextPath() + "/admin.ma");
-		
+		request.getRequestDispatcher("views/admin/adminLogin.jsp").forward(request, response);
+	
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
